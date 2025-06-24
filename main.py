4e9904d08cef50199cf4b32e2d6ce5b4051ea488
dawnnguyen-vn <dawnnguyen.ncd@gmail.com>
2025-06-24 20:49:04 +0700

    update code

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import re
+import tiktoken
 
-
-class SimpleTokenizerV1:
+class SimpleTokenizerV2:
     def __init__(self, vocab):
         self.str_to_int = vocab
         self.int_to_str = {i:s for s,i in vocab.items()}
@@ -11,12 +11,14 @@ class SimpleTokenizerV1:
         preprocessed = [
             item.strip() for item in preprocessed if item.strip()
         ]
+        preprocessed = [item if item in self.str_to_int else "<|unk|>" for item in preprocessed]
+
         ids = [self.str_to_int[s] for s in preprocessed]
         return ids
 
     def decode(self, ids):
         text = " ".join([self.int_to_str[i] for i in ids])
-        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        text = re.sub(r'\s+([,.:;?!"()\'])', r'\1', text)
         return text
 
 
@@ -28,6 +30,15 @@ with open("the-verdict.txt", "r", encoding="utf-8") as f:
     all_words.extend(["<|endoftext|>", "<|unk|>"])
     vocab_size = len(all_words)
     vocab = {token:integer for integer,token in enumerate(all_words)}
-    tokenizer = SimpleTokenizerV1(vocab)
-    text = "Hello, do you like tea?"
-    print(tokenizer.encode(text))
+    
+    tokenizer = SimpleTokenizerV2(vocab)
+    text1 = "Hello, do you like tea?"
+    text2 = "In the sunlit terraces of the palace."
+    text = " <|endoftext|> ".join((text1, text2))
+
+    # tokenizer = SimpleTokenizerV2(vocab)
+    tokenizer = tiktoken.get_encoding("gpt2")
+    text = "Akwirw ier"
+    integers = tokenizer.encode(text)
+    print(integers)
+    print(tokenizer.decode(integers))
